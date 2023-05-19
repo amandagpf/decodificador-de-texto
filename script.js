db6d5be597652737194660a82206bead43a5ff07
Amanda Fernandes <amandagpf@outlook.com>
2023-05-19 10:26:10 -0300

    Commit Caracteres Proibidos
    Nesta parte do Challenge eu aprendi sobre o regex!

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ function criptografaFrase(){
 
     var fraseSecreta = document.querySelector('textarea').value;
 
-    var fraseCriptografada = fraseSecreta
+    if(!caracteresEspeciais(fraseSecreta) && !letrasMaiusculas(fraseSecreta) && !letrasAcentuadas(fraseSecreta)){
+        var fraseCriptografada = fraseSecreta
         .replace(/e/g, 'enter')
         .replace(/i/g, 'imes')
         .replace(/a/g, 'ai')
@@ -22,6 +23,10 @@ function criptografaFrase(){
 
     var gato = document.querySelector('.resultado-img');
     gato.style.width = "200px";
+    }else{
+        alert("A sua mensagem contém caracteres inválidos, verifique e tente novamente!");
+    }
+    
 }
 
 criptografar.addEventListener('click', criptografaFrase);
@@ -30,22 +35,41 @@ function descriptografafaFrase(){
 
     var fraseSecreta = document.querySelector('textarea').value;
 
-    var fraseDescriptografada = fraseSecreta
+    if(!caracteresEspeciais(fraseSecreta) && !letrasMaiusculas(fraseSecreta) && !letrasAcentuadas(fraseSecreta)){
+        var fraseDescriptografada = fraseSecreta
         .replace(/enter/g, 'e')
         .replace(/imes/g, 'i')
         .replace(/ai/g, 'a')
         .replace(/ober/g, 'o')
         .replace(/ufat/g, 'u');
-   
-    var resultado = document.querySelector('.frase-resultado');
-    resultado.innerHTML = fraseDescriptografada;
-    document.querySelector('textarea').value = '';
 
-    resultado.style.fontSize = "30px";
-    resultado.style.marginTop = "20px";
-
-    var gato = document.querySelector('.resultado-img');
-    gato.style.width = "200px";
+        var resultado = document.querySelector('.frase-resultado');
+        resultado.innerHTML = fraseDescriptografada;
+        document.querySelector('textarea').value = '';
+    
+        resultado.style.fontSize = "30px";
+        resultado.style.marginTop = "20px";
+    
+        var gato = document.querySelector('.resultado-img');
+        gato.style.width = "200px";
+    }else{
+        alert("A sua mensagem contém caracteres inválidos, verifique e tente novamente!");
+    }
 }
 
 descriptografar.addEventListener('click', descriptografafaFrase);
+
+function caracteresEspeciais(frase){
+    var regex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?0123456789]/;
+    return regex.test(frase);
+}
+
+function letrasMaiusculas(frase){
+    var regex = /[A-Z]/;
+    return regex.test(frase);
+}
+
+function letrasAcentuadas(frase){
+    var regex = /[ÁÉÍÓÚÀÈÌÒÙÂÊÎÔÛÄËÏÖÜÃÕ]/i;
+    return regex.test(frase);
+}
